3d6c863014c96f6b75b1527609381c2017aa644b
wataridmm <r.watari.watari@gmail.com>
2021-11-13 22:10:34 +0000

    edit venues table

--- db/migrate/20211113212644_create_requests.rb
+++ db/migrate/20211113212644_create_requests.rb
@@ -2,8 +2,8 @@ class CreateRequests < ActiveRecord::Migration[5.2]
   def change
     create_table :requests do |t|
 
-       t.integer :worker_id, null: false
-       t.integer :status, null: false
+      t.integer :worker_id, null: false
+      t.integer :status, null: false
 
       t.timestamps
     end
