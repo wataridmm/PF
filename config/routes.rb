72b614ade75a8c4983d4014d975d4c28ea0c2e05
wataridmm <r.watari.watari@gmail.com>
2021-11-13 18:32:04 +0000

    edit routes devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :workers, controllers: {
+  devise_for :worker, skip: [:registrations, :passwords] , controllers: {
     sessions: "worker/sessions"
   }
-  devise_for :publics, controllers: {
+  devise_for :publics, skip: [:passwords] ,controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
-  devise_for :admin, controllers: {
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
