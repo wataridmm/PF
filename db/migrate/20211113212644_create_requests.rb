c629a4ee6cdaef0a2139235013e273b4d920184d
wataridmm <r.watari.watari@gmail.com>
2021-11-13 22:01:01 +0000

    edit request table

--- db/migrate/20211113212644_create_requests.rb
+++ db/migrate/20211113212644_create_requests.rb
@@ -2,6 +2,9 @@ class CreateRequests < ActiveRecord::Migration[5.2]
   def change
     create_table :requests do |t|
 
+       t.integer :worker_id, null: false
+       t.integer :status, null: false
+
       t.timestamps
     end
   end
