36bfc3611d8a318ab24d65ec6cc85795d6fc4c45
wataridmm <r.watari.watari@gmail.com>
2021-11-13 20:28:11 +0000

    fix devise setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+
   namespace :admin do
     get 'top' => 'homes#top', as: 'top'
   end
   devise_for :worker, skip: [:registrations, :passwords] , controllers: {
     sessions: "worker/sessions"
   }
-  devise_for :publics, skip: [:passwords] ,controllers: {
+
+  devise_for :customers, skip: [:passwords] ,controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
--- db/migrate/20211113174642_devise_create_workers.rb
+++ db/migrate/20211113174642_devise_create_workers.rb
@@ -32,6 +32,10 @@ class DeviseCreateWorkers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      t.string :last_name, null: false
+      t.string :first_name, null: false
+      t.boolean :is_deleted, null: false, default: false
+
 
       t.timestamps null: false
     end
