ac4b3909706ce54e6b774403c05e8caacf20d0be
wataridmm <r.watari.watari@gmail.com>
2021-11-13 20:50:10 +0000

    add public/top

---
+++ app/controllers/public/customers_controller.rb
@@ -0,0 +1,7 @@
+class Public::CustomersController < ApplicationController
+  
+  def top
+    
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,9 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
+
+  scope module: :public do
+    root 'customers#top'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
