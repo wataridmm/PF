66594e15686e2bd325792acc33cea9e14816226f
wataridmm <r.watari.watari@gmail.com>
2021-11-13 13:04:40 +0900

    edit devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :workers
-  devise_for :publics
-  devise_for :admins
+  devise_for :workers, controllers: {
+    sessions: "worker/sessions"
+  }
+  devise_for :publics, controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+  devise_for :admin, controllers: {
+    sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
