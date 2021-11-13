711007503d4e63c917f2f202bc171377973cc796
wataridmm <r.watari.watari@gmail.com>
2021-11-13 18:45:00 +0000

    add controller admin/homes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'homes/top'
+  end
   devise_for :worker, skip: [:registrations, :passwords] , controllers: {
     sessions: "worker/sessions"
   }
