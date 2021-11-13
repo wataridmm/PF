c2af2a3df3ee9922022a11bed5c52a95589eef17
wataridmm <r.watari.watari@gmail.com>
2021-11-13 21:57:44 +0000

    edit item_stocks, request_details table

--- db/migrate/20211113212550_create_item_stocks.rb
+++ db/migrate/20211113212550_create_item_stocks.rb
@@ -2,6 +2,12 @@ class CreateItemStocks < ActiveRecord::Migration[5.2]
   def change
     create_table :item_stocks do |t|
 
+      t.integer :item_id, null: false
+      t.integer :stock, null: false
+      t.integer :status, null: false
+
+
+
       t.timestamps
     end
   end
