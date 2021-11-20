9a30debf63dfc37b2b76c63a2de02ee89f16df40
wataridmm <r.watari.watari@gmail.com>
2021-11-20 10:08:56 +0000

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -44,10 +44,18 @@ Rails.application.routes.draw do
   namespace :admin do
     get 'top' => 'homes#top', as: 'top'
   end
+  devise_for :worker, skip: [:passwords] , controllers: {
+    registrations: "worker/registrations",
+    sessions: "worker/sessions"
+  }
 
-  devise_for :customers
-  devise_for :worker
-  devise_for :admin
+  devise_for :customers, skip: [:passwords] ,controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
 
   scope module: :public do
     root 'customers#top'
