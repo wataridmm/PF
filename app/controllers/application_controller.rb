7cfbeaf37458562aa160f7d833ae12be276d741b
wataridmm <r.watari.watari@gmail.com>
2021-11-20 09:21:38 +0000

    fix session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name, :name])
   end
-
 
 
 end
