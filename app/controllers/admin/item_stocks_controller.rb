dc1b668677508da3d48b91f3b57d9c7ca2144a39
wataridmm <r.watari.watari@gmail.com>
2021-11-21 07:37:05 +0000

    add item_stocks controller

---
+++ app/controllers/admin/item_stocks_controller.rb
@@ -0,0 +1,7 @@
+class Admin::ItemStocksController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
   namespace :admin do
+    get 'item_stocks/index'
+    get 'item_stocks/show'
+  end
+  namespace :admin do
     get 'customers' => 'customers#index'
     get 'customers/:id/edit' => 'customers#edit', as: 'edit_customer'
     patch 'customers/:id' => 'customers#update', as: 'update_customer'
