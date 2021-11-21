6545700799f12182bcf5a9918f61a48ba6272552
wataridmm <r.watari.watari@gmail.com>
2021-11-21 08:10:54 +0000

    git diff

--- app/controllers/admin/item_stocks_controller.rb
+++ app/controllers/admin/item_stocks_controller.rb
@@ -1,7 +1,30 @@
 class Admin::ItemStocksController < ApplicationController
   def index
+    @item_stocks = Item_stocks.all
   end
 
   def show
+
   end
+
+  def new
+    @item_stocks = Item_stocks.new
+  end
+
+  def create
+    item_stock = item_stock.new(item_stock_params)
+    item_stock.save
+    redirect_to admin_itemstocks_path
+  end
+
+  private
+
+  def item_stock_params
+    params.require(:item_stock).permit(:item_id, :stock, :status)
+  end
+
+
+
+
+
 end
