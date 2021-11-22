90c1d1506acb287330297b49647098746892921a
wataridmm <r.watari.watari@gmail.com>
2021-11-22 11:51:14 +0000

    fix controller,migrate

--- app/controllers/worker/requests_controller.rb
+++ app/controllers/worker/requests_controller.rb
@@ -1,10 +1,22 @@
 class Worker::RequestsController < ApplicationController
   def new
+    @request = Request.new
   end
 
   def index
+    @requests = requests.all
+    @worker = current_worker.id
+    @requests = Request.where(worker_id: @worker)
+
   end
+  
+  def all_workers
+    
+  end
+  
 
   def show
+    @worker = Worker.find(params[:id])
+    @requests = @worker.requests.page(params[:page]).reverse_order
   end
 end
