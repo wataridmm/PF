2b06d459f0761fa2cec43d7fe7fe50674f610a73
wataridmm <r.watari.watari@gmail.com>
2021-11-22 14:13:28 +0000

    edit controller

--- db/migrate/20211113212644_create_requests.rb
+++ db/migrate/20211113212644_create_requests.rb
@@ -3,7 +3,7 @@ class CreateRequests < ActiveRecord::Migration[5.2]
     create_table :requests do |t|
 
       t.integer :worker_id, null: false
-      t.integer :status, null: false
+      t.integer :status, null: false, default: 0
       t.integer :venue_id
       t.timestamps
     end
