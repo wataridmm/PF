cda8755df262459a5753ba214ffe1df454194579
wataridmm <r.watari.watari@gmail.com>
2021-11-23 09:13:16 +0000

    add event destroy

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -13,8 +13,28 @@ class Admin::EventsController < ApplicationController
   end
 
   def show
+    @event = Event.find(params[:id])
   end
 
   def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    event = Event.find(params[:id])
+    event.update(event_params)
+    redirect_to admin_event_path(event.id)
+  end
+
+  def destroy
+    event = Event.find(params[:id])
+    event.destroy
+    redirect_to admin_events_path
+  end
+
+  private
+
+  def event_params
+  params.require(:event).permit(:name, :comment, :date, :venue_id)
   end
 end
