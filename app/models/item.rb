1a16d936d921a937c481965aae5d828c473d0322
wataridmm <r.watari.watari@gmail.com>
2021-11-23 07:25:26 +0000

    edit request new

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,6 @@ class Item < ApplicationRecord
 
   has_many :request_details, dependent: :destroy
   has_many :item_stocks
+  
 
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,5 +1,6 @@
 class Request < ApplicationRecord
     belongs_to :worker
-
-
+    has_many :request_details
+    belongs_to :request
+    belongs_to :item
 end
