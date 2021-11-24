478ebe7135e1156b1493e377214470ffe4e4d44c
wataridmm <r.watari.watari@gmail.com>
2021-11-24 03:53:32 +0000

    fix request new

--- db/schema.rb
+++ db/schema.rb
@@ -80,7 +80,7 @@ ActiveRecord::Schema.define(version: 2021_11_13_212757) do
 
   create_table "requests", force: :cascade do |t|
     t.integer "worker_id", null: false
-    t.integer "status", default: 0, null: false
+    t.integer "status", limit: 1, default: 0, null: false
     t.integer "venue_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
