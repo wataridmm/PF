177f06e68965b95f468bc037205e0e0168264413
wataridmm <r.watari.watari@gmail.com>
2021-12-04 12:04:05 +0000

    add event commnt

---
+++ app/helpers/public/event_comments_helper.rb
@@ -0,0 +1,2 @@
+module Public::EventCommentsHelper
+end
---
+++ db/migrate/20211204114922_create_event_comments.rb
@@ -0,0 +1,16 @@
+class CreateEventComments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :event_comments do |t|
+
+
+
+      t.integer "customer_id"
+      t.text "comment", null: false
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.decimal "score", precision: 5, scale: 3
+
+      t.timestamps
+    end
+  end
+end
