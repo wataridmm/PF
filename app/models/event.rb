3ccd5eca0e1e40ee56bd027f88e9cb2085cb8ff6
wataridmm <r.watari.watari@gmail.com>
2021-12-06 12:20:02 +0000

    add comment average

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@ class Event < ApplicationRecord
   belongs_to :venue
   has_many :requests
   belongs_to :worker, optional: true
+  has_many :event_comments
 
   validates :venue_id, presence: true
   validates :name, presence: true
